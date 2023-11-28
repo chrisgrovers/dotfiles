28845ba130faab6a943eacb4fff7d17ed462b772
chrisgrovers <chrisgrovers@gmail.com>
2023-11-28 09:09:29 -0800

    better indentation in vim

--- .config/nvim/remaps.lua
+++ .config/nvim/remaps.lua
@@ -73,4 +73,6 @@ vim.keymap.set('n', ']b', '<cmd>bnext<cr>', { noremap = true })
 -- { { Move highlighted lines } }
 vim.api.nvim_set_keymap('v', '<S-J>', ':m \'>+1<CR>gv', { silent = true })
 vim.api.nvim_set_keymap('v', '<S-K>', ':m \'<-2<CR>gv', { silent = true })
+vim.api.nvim_set_keymap('v', '<', '<gv', { silent = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { silent = true })
 
